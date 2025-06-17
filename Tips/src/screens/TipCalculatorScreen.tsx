f1646d514829a11cca28595e6a2d6e94a2514f80
fine <92whdtjs@gmail.com>
2025-06-17 14:34:36 +0900

    배경색 변경

--- Tips/src/screens/TipCalculatorScreen.tsx
+++ Tips/src/screens/TipCalculatorScreen.tsx
@@ -21,42 +21,45 @@ export const TipCalculatorScreen: React.FC = () => {
   } = useTipCalculator();
 
   return (
-    <SafeAreaView className="flex-1 bg-gradient-to-br from-blue-50 to-indigo-100">
-      <View className="flex-1 justify-center p-4">
-        <View className="space-y-4">
-          {/* Calculator Display */}
-          <View className="h-[80px] mb-4">
-            <CalculatorDisplay currentInput={currentInput} />
-          </View>
-          
-          {/* NumberPad */}
-          <View className="h-[360px] mb-4">
-            <NumberPad
-              onDigit={addDigit}
-              onDelete={deleteDigit}
-              onClear={clear}
-              onAdd={addItem}
-            />
-          </View>
-          
-          {/* TipSelector */}
-          <View className="h-[70px] mb-4">
-            <TipSelector
-              selectedTip={selectedTip}
-              onTipChange={setSelectedTip}
-            />
-          </View>
-          
-          {/* ResultSummary */}
-          <View className="h-[180px]">
-            <ResultSummary
-              subtotal={calculations.subtotal}
-              tipAmount={calculations.tipAmount}
-              total={calculations.total}
-              selectedTip={selectedTip}
-              items={items}
-              onResetItems={resetItems}
-            />
+    <SafeAreaView className="flex-1 bg-slate-50">
+      <View className="flex-1 bg-slate-50">
+        {/* Content */}
+        <View className="flex-1 justify-center p-4">
+          <View className="space-y-4">
+            {/* Calculator Display */}
+            <View className="h-[80px] mb-4">
+              <CalculatorDisplay currentInput={currentInput} />
+            </View>
+            
+            {/* NumberPad */}
+            <View className="h-[360px] mb-4">
+              <NumberPad
+                onDigit={addDigit}
+                onDelete={deleteDigit}
+                onClear={clear}
+                onAdd={addItem}
+              />
+            </View>
+            
+            {/* TipSelector */}
+            <View className="h-[70px] mb-4">
+              <TipSelector
+                selectedTip={selectedTip}
+                onTipChange={setSelectedTip}
+              />
+            </View>
+            
+            {/* ResultSummary */}
+            <View className="h-[180px]">
+              <ResultSummary
+                subtotal={calculations.subtotal}
+                tipAmount={calculations.tipAmount}
+                total={calculations.total}
+                selectedTip={selectedTip}
+                items={items}
+                onResetItems={resetItems}
+              />
+            </View>
           </View>
         </View>
       </View>
